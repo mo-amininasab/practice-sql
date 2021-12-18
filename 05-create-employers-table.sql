61fefbfa6958d9377b76cf7b399d273f7c597f8d
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-12-19 01:58:15 +0330

    table employers created

--- 05-create-employers-table.sql
+++ 05-create-employers-table.sql
@@ -1,6 +1,8 @@
 CREATE TABLE employers (
   company_name VARCHAR(255) NOT NULL,
   company_address VARCHAR(300) NOT NULL,
-  yearly_revenue FLOAT(5, 2) NOT NULL -- Allowed: 123.12, 12.1, Not allowed: 1234.12, 1.123
-
+  -- for FLOAT, paratethis are not nessesary
+  -- yearly_revenue FLOAT(5,2) NOT NULL, -- Approximatino, Allowed: 123.12, 12.1, Not allowed: 1234.12, 1.123
+  yearly_revenue NUMERIC(5,2) NOT NULL, --Exact value, Allowed: 123.12, Not allowed: 1234.12, 1.123
+  is_hiring BOOLEAN NOT NULL
 );
