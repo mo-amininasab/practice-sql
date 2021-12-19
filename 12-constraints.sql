5bf089996acfcd4be2748701d20dcfee37ac9ae6
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-12-19 12:35:42 +0330

    deal with null values

---
+++ 12-constraints.sql
@@ -0,0 +1,10 @@
+-- CREATE TABLE users (
+--   full_name VARCHAR(300) NOT NULL,
+--   -- ...
+-- );
+
+ALTER TABLE users
+ALTER COLUMN full_name SET NOT NULL, -- PostgreSQL
+ALTER COLUMN current_status SET NOT NULL; -- PostgreSQL
+-- MODIFY COLUMN full_name VARCHAR(300) NOT NULL, -- MySQL
+-- MODIFY COLUMN current_status ENUM('employed', 'self-employed', 'unemployed') NOT NULL; -- MySQL
