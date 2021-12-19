1207fd4c97214a52eb4889546d2a575e13b5a8e4
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-12-19 14:16:25 +0330

    data inserted

--- 02-data-manipulation(CRUD)/01-basic-table-setup.sql
+++ 02-data-manipulation(CRUD)/01-basic-table-setup.sql
@@ -1,14 +1,14 @@
 -- CREATE DATABASE sales;
 
 CREATE TABLE sales (
-  id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
-  -- id SERIAL PRIMARY KEY, -- PostgreSQL
-  date_created DATE DEFAULT (CURRENT_DATE), -- MySQL
-    -- date_created DATE DEFAULT CURRENT_DATE, -- PostgreSQL
+  -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+  id SERIAL PRIMARY KEY, -- PostgreSQL
+  -- date_created DATE DEFAULT (CURRENT_DATE), -- MySQL
+    date_created DATE DEFAULT CURRENT_DATE, -- PostgreSQL
   date_fulfilled DATE,
   customer_name VARCHAR(300) NOT NULL,
   product_name VARCHAR(300) NOT NULL,
   volume NUMERIC(10,3) NOT NULL CHECK (volume >= 0),
   is_recurring BOOLEAN DEFAULT FALSE,
-  is_disbuted BOOLEAN DEFAULT FALSE
+  is_disputed BOOLEAN DEFAULT FALSE
 );
