9686a8ea23babe7df94967f11f7733f4d5349faf
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-12-19 16:26:41 +0330

    SELECT featurs

--- 02-data-manipulation(CRUD)/06-select-data.sql
+++ 02-data-manipulation(CRUD)/06-select-data.sql
@@ -2,5 +2,30 @@
 -- SELECT *
 -- FROM sales;
 
-SELECT customer_name, product_name, volume, volume, date_created
+-- SELECT customer_name, product_name, volume, date_created
+-- FROM sales;
+
+-- alias
+-- SELECT customer_name,
+--   product_name,
+--   volume AS total_sales,
+--   date_created
+-- FROM sales;
+
+-- adding data to every column
+-- SELECT 
+--   'Hello World',
+--   15000,
+--   customer_name,
+--   product_name,
+--   volume,
+--   date_created
+-- FROM sales;
+
+-- arithmetic
+SELECT 
+  customer_name,
+  product_name,
+  volume / 1000,
+  date_created
 FROM sales;
