4a4a9795b1be0fa171ba6b6abf748112aa15ff18
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-12-19 19:15:35 +0330

    INNER JOIN users, addresses, cities

--- 03-data-relationships/04-inner-join.sql
+++ 03-data-relationships/04-inner-join.sql
@@ -4,6 +4,7 @@ SELECT u.id,
   u.email,
   a.street,
   a.house_number,
-  city_id
+  c.name AS city_name
 FROM users AS u
-  INNER JOIN addresses AS a ON u.address_id = a.id;
+  INNER JOIN addresses AS a ON u.address_id = a.id
+  INNER JOIN cities AS c ON a.city_id = c.id;
