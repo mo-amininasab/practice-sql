3e869a7234ce731154800589565cadcafc19ede8
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-12-19 18:47:36 +0330

    created tables

---
+++ 03-data-relationships/02-create-tables.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users (
+  -- MySQL
+  -- id INT PRIMARY KEY AUTO_INCREMENT,
+  -- PostgreSQL
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  address_id INT NOT NULL
+);
+CREATE TABLE addresses (
+  -- MySQL
+  -- id INT PRIMARY KEY AUTO_INCREMENT,
+  -- PostgreSQL
+  id SERIAL PRIMARY KEY,
+  street VARCHAR(255) NOT NULL,
+  house_number VARCHAR(50) NOT NULL,
+  city_id INT NOT NULL
+);
+CREATE TABLE city (
+  -- MySQL
+  -- id INT PRIMARY KEY AUTO_INCREMENT,
+  -- PostgreSQL
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
