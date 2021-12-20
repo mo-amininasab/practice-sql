cfae2aba198fe98269c12d0572f30340c5bf2a9a
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-12-20 14:07:33 +0330

    therse a problem with this

--- 03-data-relationships/03-insert-data.sql
+++ 03-data-relationships/03-insert-data.sql
@@ -5,15 +5,15 @@ VALUES ('Munich'),
   ('Washington, D.C.');
 
 INSERT INTO addresses (street, house_number, city_id)
-VALUES ('Beerstreet', '91', 4),
-  ('Beerstreet', '12', 4),
-  ('Pizzastreet', '1', 5),
-  ('Burgerstreet', '9', 2),
-  ('Anotherstreet', '12', 1),
-  ('Smallstreet', '11', 3);
+VALUES ('Beerstreet', '91', 9),
+  ('Beerstreet', '12', 9),
+  ('Pizzastreet', '1', 10),
+  ('Burgerstreet', '9', 12),
+  ('Anotherstreet', '12', 10),
+  ('Smallstreet', '11', 11);
   
 INSERT INTO users (first_name, last_name, email, address_id)
-VALUES ('Marina', 'Marks', 'marina@test.com', 2),
-  ('Hans', 'Mayer', 'hansm@test.com', 5),
-  ('Maria', 'Marionatti', 'maria@test.com', 7),
-  ('Michael', 'Smith', 'michael@test.com', 8);
+VALUES ('Marina', 'Marks', 'marina@test.com', 25),
+  ('Hans', 'Mayer', 'hansm@test.com', 26),
+  ('Maria', 'Marionatti', 'maria@test.com', 27),
+  ('Michael', 'Smith', 'michael@test.com', 28);
